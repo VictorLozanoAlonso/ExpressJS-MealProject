780b212efda82c1e04f75a53cf8ddce89a80b3fa
Victor Lozano Alonso <victor@MacBook-Pro-de-Victor.local>
2021-10-27 11:04:23 -0500

    Sign-up form built with field validation

--- controllers/forms.js
+++ controllers/forms.js
@@ -17,6 +17,35 @@
          title: "Sign Up"
      });
  });
+ router.post("/sign-up", (req, res) => {
+    const { fName, lName, email, password } = req.body;
+    const regexEmail = /^(([^<>\/()\º\ª$%'!?=·#*{}[\]\\.,;:\s@"]+(\.[^<>\/()\º\ª$%'!?=·#*{}[\]\\.,;:\s@"]+)*)|(".+"))@((([a-zA-Z\-0-9]{3,})+\.)+[a-zA-Z]{2,})$/;
+    let passed = true;
+    let validation = {};
+    if (fName === null || fName.length === 0){
+        passed = false;
+        validation.fName = "*First Name cannot be null or empty";
+    }
+    if (lName === null || lName.length === 0){
+        passed = false;
+        validation.lName = "*Last Name cannot be null or empty";
+    }
+    if (email.search(regexEmail) === -1){
+        passed = false;
+        validation.email = "*Format is incorrect. You should use something like email@email.com"
+    }
+    if (password.search(/\d/) === -1 || password.search(/\W/) === -1 || password.search(/[a-z]/) === -1 || password.search(/[A-Z]/) === -1 || password.length < 6 || password.length > 12){
+        passed = false;
+        validation.password = "*Password must have 6 to 12 characters and contains at least one lowercase letter, uppercase letter, number and symbol"
+    }
+    if(!passed){
+        res.render("forms/signup", {
+            title: "Sign Up",
+            values: req.body,
+            validation
+        });
+    }
+});
  router.get("/login", function(req, res) {
      res.render("forms/login", {
          title: "Login"
